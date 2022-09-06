232100ff4b4fa8d0b647b968ae81b1948efdd77e
Antony Leme <antony.leme@rbrdigital.io>
2022-09-06 16:19:49 -0300

    feat: project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Image, Flex, FormControl, FormLabel, Input, Button, Stack, HStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import degrade from './assets/degrade.png';
+import OgJPG from './assets/fundojpg.jpg'
+import LogoSVG from './assets/logo.svg'
+import * as htmlToImage from 'html-to-image';
 
 function App() {
+  const [title, setTitle] = useState('Page Title');
+  const [path, setPath] = useState('path-here')
+  const [bg, setBg] = useState('');
+
+  const toBase64 = async (file) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+  
+  const updateFile = async function(file){
+    const data = await toBase64(file);
+    setBg(data);
+  }
+
+  const printRef = React.useRef();
+
+  const handleDownloadImage = async () => {
+
+    htmlToImage.toPng(document.getElementById('og-wrapper'))
+    .then(function (dataUrl) {
+      var link = document.createElement('a');
+      link.download = `${path}.png`;
+      link.href = dataUrl;
+      link.click();
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p='32px'>
+      <Box p='32px' mb='32px' w='700px' mx='auto'>
+        <Stack spacing='16px'>
+          <HStack>
+            <FormControl>
+              <FormLabel>Titulo</FormLabel>
+              <Input value={title} onChange={e => setTitle(e.target.value)}/>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Path</FormLabel>
+              <Input value={path} onChange={e => setPath(e.target.value)}/>
+            </FormControl>
+          </HStack>
+          <FormControl>
+            <FormLabel>Imagem</FormLabel>
+            <Input type='file' onChange={e => e.target.files.length ? updateFile(e.target.files[0]) : null}/>
+          </FormControl>
+          <Button onClick={handleDownloadImage} type='submit' colorScheme='blue'>Download</Button>
+        </Stack>
+      </Box>
+
+    <Flex justifyContent='center'>
+      <Box ref={printRef} id='og-wrapper'>
+        <Box pos='relative' w='1200px'>
+          <Image position='absolute' src={LogoSVG} zIndex='10000'/>
+          <Image src={OgJPG}/>
+          <Flex 
+            textAlign='center' position='absolute'
+            top='0'
+            right='0'
+            bottom='0'
+            left='0'
+            zIndex='200'
+            color='white'
+            justifyContent='center'
+            alignItems='center'
+            fontFamily='Roboto'
+          >
+            <Box textAlign='center' pt='70px' position='relative' zIndex='10000'>
+              <Box fontWeight='600' fontSize='80px'>
+                {title}
+              </Box>
+              <Box mb='12px' mx='auto' w='116%' h='2px' bg='white' ml='-8%'/>
+              <Box whiteSpace='nowrap' textTransform={'uppercase'} fontSize='35px'>
+                WWW.AFLDS.ORG/<strong>{path}</strong>
+              </Box>
+            </Box>
+          </Flex>
+
+          <Box left='calc(50% - 11.5px)' transform={'translateX(-50%)'} position='absolute' bottom='0' 
+          w='948px' h='578px' bgImage={bg} bgSize='100%'/>
+          <Image 
+            pos='absolute' 
+            top='0' 
+            left='0' 
+            right='0' 
+            bottom='0' 
+            src={degrade}
+            zIndex='100'
+            blendMode={'multiply'}
+          />
+        </Box>
+      </Box>
+    </Flex>
+    </Box>
   );
 }
 
